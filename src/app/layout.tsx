b5367dbe4ae49d45d08302faf3df790845fd844b
Mauricio Dulce <mauricio@dulce.dev>
2024-07-18 12:03:25 -0500

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import "@/styles/globals.css";
+import { PrismicPreview } from "@prismicio/next";
+
+import { repositoryName } from "@/prismicio";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
